7cf226a982e92d130b5292a3863a6bd1b0ebf9fb
danielcasanova12 <119464431+danielcasanova12@users.noreply.github.com>
2024-03-29 14:47:39 -0300

    Feat: message

--- Pm.py
+++ Pm.py
@@ -8,28 +8,36 @@ def calcular_preco_medio(arquivo):
     
     # df = df[df.groupby('Produto')['Quantidade'].transform('sum') >= 0]
     df = df[df.groupby('Produto')['Quantidade'].transform('sum') > 0]
+
     # Calcular o preço médio para cada ativo
     df['Preço Médio'] = df.groupby('Produto')['Valor da Operação'].transform('sum') / df.groupby('Produto')['Quantidade'].transform('sum')
     
+    df['Quantidade_total'] = df.groupby('Produto')['Quantidade'].transform('sum')
     # Remover linhas duplicadas
     df = df.drop_duplicates(subset='Produto')
     df.drop('Entrada/Saída', axis=1, inplace=True)
     df.drop('Data', axis=1, inplace=True)
     df.drop('Movimentação', axis=1, inplace=True)
     df.drop('Instituição', axis=1, inplace=True)
+    df.drop('Preço unitário', axis=1, inplace=True)
+    df.drop('Quantidade', axis=1, inplace=True)
     df.drop('Valor da Operação', axis=1, inplace=True)
     df['Produto'] = df['Produto'].str.split(' ').str[0]
 
     return df
 
+def formatar_mensagem(row):
+    return f"{row['Produto']} quantidade {row['Quantidade_total']} e com preço médio de R$ {row['Preço Médio']:.2f}"
+
 def formatar_cnpj(cnpj):
+    print(cnpj)
     return str(cnpj).zfill(14)
 
 def completar_tabela(arquivo, resultado):  
     df = pd.read_excel(arquivo)
     df['Produto'] = df['Produto'].str.split(' ').str[0]
-    df['CNPJ da Empresa'] = df['CNPJ da Empresa'].apply(formatar_cnpj)
-    
+    df['CNPJ da Empresa'] = df['CNPJ da Empresa']
+    formatar_cnpj(df['CNPJ da Empresa'])
     # Merge dos dois DataFrames
     resultado_final = pd.merge(resultado, df[['Produto', 'CNPJ da Empresa']], on='Produto', how='left')
     
@@ -41,4 +49,6 @@ resultado = calcular_preco_medio(arquivo)
 arquivo2 = 'relatorio-consolidado-anual-2023.xlsx'
 resultado_final = completar_tabela(arquivo2, resultado)
 
-resultado_final.to_excel('resultado.xlsx')
+resultado_final['Mensagem'] = resultado_final.apply(formatar_mensagem, axis=1)
+
+resultado_final.to_excel('resultado2.xlsx', index=False)
